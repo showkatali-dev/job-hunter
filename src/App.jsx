6a36d7ed3389e89275d47dd3ab06da0049860431
coder-showkat <showkat.coder@gmail.com>
2023-04-09 21:46:58 +0600

    react-router setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import AppliedJobs from "./pages/AppliedJobs";
+import Blogs from "./pages/Blogs";
+import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
+import JobDetails from "./pages/JobDetails";
+import Statistics from "./pages/Statistics";
+
+const Layout = () => {
+  return (
+    <>
+      <header></header>
+      <main>
+        <Outlet />
+      </main>
+      <footer></footer>
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/job/:id",
+        element: <JobDetails />,
+      },
+      {
+        path: "/statistics",
+        element: <Statistics />,
+      },
+      {
+        path: "/applied-jobs",
+        element: <AppliedJobs />,
+      },
+      {
+        path: "/blog",
+        element: <Blogs />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
